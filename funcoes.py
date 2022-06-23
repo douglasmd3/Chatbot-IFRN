1107f978ad3f64dc9436b94005d92bef873c52a4
douglasmd3 <74698250+douglasmd3@users.noreply.github.com>
2022-06-23 15:41:33 -0300

    adiciona botoes e imagem de boas vindas

---
+++ funcoes.py
@@ -0,0 +1,100 @@
+from telegram import Update
+from telegram.ext import CallbackContext, Updater, MessageHandler, Filters, CallbackQueryHandler
+import logging
+import texto, botoes
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext) -> None:
+    context.bot.send_photo(
+        chat_id=update.effective_message.chat_id,
+        photo=open('Imagens/Imagem-Inicial.jpg', 'rb'),
+        caption=f'Olá, {update.effective_user.full_name}! que ótimo ter você por aqui 😀'
+    )
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=texto.start_texto,
+        reply_markup=botoes.start_lines
+    )
+def balloon (update: Update, context: CallbackContext) -> None:
+    query = update.callback_query.data
+    update.callback_query.answer()
+
+    handler = update.callback_query
+    handler.answer()
+
+    if "HOMER" in query:
+        handler.edit_message_text(
+            text=f'Olá, {update.effective_user.full_name}! ' + texto.start_texto,
+            reply_markup=botoes.start_lines
+        )
+    if "VOLTAR_SETOR_LINE" in query:
+        handler.edit_message_text(
+            text="Escolha uma opção disponível para continuar 👇",
+            reply_markup=botoes.setor_line
+        )
+    if "VOLTAR_FAQ_SEAC" in query:
+        handler.edit_message_text(
+            text=texto.txt_seac + texto.FAQ,
+            reply_markup=botoes.faq_seac
+        )
+    if "Estrutura_Administrativa" in query:
+        handler.edit_message_text(
+            text="Escolha uma opção disponível para continuar 👇",
+            reply_markup=botoes.setor_line
+        )
+    if "SEAC/SGA" in query:
+        handler.edit_message_text(
+            text=texto.txt_seac,
+            reply_markup=botoes.menu_seac
+        )
+    if "Contato_seac" in query:
+        handler.edit_message_text(
+            text=texto.txt_seac + texto.seac_contato,
+            reply_markup=botoes.regressar_setor_line
+        )
+    if "COEX/SGA" in query:
+        handler.edit_message_text(
+            text=texto.txt_coex,
+            reply_markup=botoes.menu_coex
+        )
+    if "Contato_coex" in query:
+        handler.edit_message_text(
+            text=texto.txt_coex + texto.coex_contato,
+            reply_markup=botoes.regressar_setor_line
+        )
+    if "FAQ_seac" in query:
+        handler.edit_message_text(
+            text=texto.txt_seac + texto.FAQ,
+            reply_markup=botoes.faq_seac
+        )
+    if "faq_seac1" in query:
+        handler.edit_message_text(
+            text=texto.txt_faq_seac1,
+            reply_markup=botoes.regressar_faq_seac
+        )
+    if "faq_seac2" in query:
+        handler.edit_message_text(
+            text=texto.txt_faq_seac2,
+            reply_markup=botoes.regressar_faq_seac
+        )
+    if "FAQ_coex" in query:
+        handler.edit_message_text(
+            text=texto.txt_coex + texto.FAQ,
+            reply_markup=botoes.regressar_setor_line
+        )
+def iniciar() -> None:
+    token = "5241177916:AAHZUC5gimNEyosHBngN5-KELqBSYauthok"
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+
+# Iniciar comandos da função quando solicitadas
+    dispatcher.add_handler(MessageHandler(Filters.all, start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(balloon))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    iniciar()
---
+++ texto.py
@@ -0,0 +1,70 @@
+start_texto = "Para prosseguir, selecione uma opção disponível que poderá lhe ajudar no seu atendimento 👇"
+
+txt_seac = "A secretaria Acadêmica | SEAC é o setor responsável pelo controle, verificação, registro e arquivamento da "
+txt_seac +="documentação da vida acadêmica do aluno, desde seu ingresso na Instituição, até a conclusão e/ou expedição do diploma.\n"
+txt_seac +="\nA missão da Secretaria Acadêmica é realizar com presteza e eficácia todos os procedimentos que envolvem o controle e o "
+txt_seac +="registro acadêmico, assim como orientar os alunos para os mesmos."
+
+seac_contato = """\n
+Responsáveis:
+Renato Silva de Oliveira (Secretário acadêmico)
+Anderson de Brito Rodrigues
+Maria Vilanir Gonçalves Duarte da Silva
+Rafael da Silva Araújo\n
+Informação para contato:
+Telefone: (84) 4005-4111
+Ramal 7406 / 7436
+E-mail: seac.sga@ifrn.edu.br
+Canal Telegram: https://t.me/secretariaacademicaifrnsga
+"""
+
+txt_coex = """A Coordenação de Extensão (COEX) tem como objetivo central garantir a interação entre o IFRN e a comunidade externa.\n"""
+txt_coex += """\nCom isso, busca garantir a divulgação, o desenvolvimento de novas tecnologias e a manutenção da relação com o mundo do trabalho para que desta forma seja efetiva a qualidade da formação dos nossos estudantes."""
+
+coex_contato = """\n
+Responsáveis:
+Fernando de Oliveira Freire (Coordenador)\n
+Informação para contato:
+Telefone: (84) 4005-4111 - Ramal 7410
+E-mail: 
+coex.sga@ifrn.edu.br 
+extensão.sga@gmail.com
+Canal Telegram: https://t.me/coordenacaodeextensaoifrnsga
+Grupo WhatsApp: https://chat.whatsapp.com/LNw9BvA5qsV0H3QOeypzDK
+Instagram: https://www.instagram.com/coex.sga/
+Link Coex: https://linktr.ee/coex.sga
+"""
+
+FAQ = """
+\nOlá, caro(a) requerente 🤗 há alguma dúvida para esclarecer ❓ Aqui segue algumas das perguntas frequentes do setor selecionado.
+veja e selecione aquela que melhor atende sua expectativa. Espero que isso lhe ajude em sua busca.
+Caso não encontre o que procura, minhas recomendações são:
+1 - Entre em contato com o setor 🏢
+2 - Entre em contato com o gerente 👤 deste sistema para sugerir 💬 alguma implementação.
+3 - Busque atendimento presencial com o setor 🏢 nos horários de funcionamento ⏱ 
+"""
+
+txt_faq_seac1 = """\n
+\nComo justificar faltas❓ ou\nComo solicitar reposição de atividades❓\n
+A solicitação deverá ser realizada através de chamado no SUAP, anexando o requerimento juntamente com os documentos comprobatórios.
+Link SUAP: https://suap.ifrn.edu.br/accounts/login/?next=/
+Link Requerimento Padrão: https://portal.ifrn.edu.br/campus/saogoncalo/arquivos/formularios-seac/requerimento-padrao/
+Com o tal requeriento é possível solicitar:
+- Matrículas;
+- Cancelamento de matrícula;
+- Renovação de matrícula (semestral ou anual);
+- Trancamento de matrícula ou disciplinas;
+- Adequação de horários / Estudo individualizado;
+- Atendimento domiciliar;
+- Dispensa de atividades;
+- Justificativa de falta;
+- Mudança de curso / turma / turno;
+- Reposição de atividades;
+- Revisão de faltas / notas / situação;
+- Transferência
+"""
+
+txt_faq_seac2 = """\n
+\nComo solicitar mudança de tuno ou turma❓\nA solicitação deverá ser realizada através de chamado no SUAP, anexando o requerimento juntamente com justificativa irrefutável da necessidade de mudança de turno ou turma.
+Para menores de 18 anos o requerimento deverá ser assinado pelo responsável juntamente com a cópia do RG(Frente e Verso) do responsável.
+"""
