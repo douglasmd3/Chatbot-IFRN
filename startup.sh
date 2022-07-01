1e8cf8b2e1fb61863acce9f2d794a12ce9101ed6
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-07-01 12:06:47 -0300

    incluindo instrucoes do heroku

--- startup.sh
+++ startup.sh
@@ -2,3 +2,8 @@
 #pip3.9 install python-telegram-bot
 #pip3.9 install gtts playsound
 python3 funcoes.py
+
+## Heroku steps
+# heroku login
+# heroku git:remote -a ifrn-sga-bot
+# tutorial => https://devcenter.heroku.com/articles/heroku-cli
