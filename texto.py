092a150721929ca914dc397d722b0daa5ed567f9
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-07-01 10:53:35 -0300

    atualiza string de texto

--- texto.py
+++ texto.py
@@ -1,9 +1,15 @@
+HOME = "HOME"
+ESTRUTURA_ADMINISTRATIVA = "ESTRUTURA_ADMINISTRATIVA"
+VOLTAR_FAQ_SEAC = "VOLTAR_FAQ_SEAC"
+SEAC_SGA = "SEAC/SGA"
+CONTATO_SEAC = "CONTATO_SEAC"
+COEX_SGA = "COEX/SGA"
+CONTATO_COEX = "CONTATO_COEX"
+FAQ_SEAC = "FAQ_SEAC"
+FAQ_COEX = "FAQ_COEX"
 start_texto = "Para prosseguir, selecione uma opção disponível que poderá lhe ajudar no seu atendimento 👇"
 
-txt_seac = "A secretaria Acadêmica | SEAC é o setor responsável pelo controle, verificação, registro e arquivamento da "
-txt_seac += "documentação da vida acadêmica do aluno, desde seu ingresso na Instituição, até a conclusão e/ou expedição do diploma.\n"
-txt_seac += "\nA missão da Secretaria Acadêmica é realizar com presteza e eficácia todos os procedimentos que envolvem o controle e o "
-txt_seac += "registro acadêmico, assim como orientar os alunos para os mesmos."
+txt_seac = """A secretaria Acadêmica | SEAC é o setor responsável pelo controle, verificação, registro e arquivamento da documentação da vida acadêmica do aluno, desde seu ingresso na Instituição, até a conclusão e/ou expedição do diploma.\n\nA missão da Secretaria Acadêmica é realizar com presteza e eficácia todos os procedimentos que envolvem o controle e o registro acadêmico, assim como orientar os alunos para os mesmos."""
 
 seac_contato = """\n
 Responsáveis:
