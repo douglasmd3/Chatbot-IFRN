21af5ac5cf70962be464baa2cd12d5849807bf2d
douglasmd3 <74698250+douglasmd3@users.noreply.github.com>
2022-08-11 00:18:40 -0300

    comentario

--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ from bot.telegram.ClienteTelegram import ClienteTelegram
 if __name__ == "__main__":
     clienteTelegram = ClienteTelegram()
     clienteTelegram.iniciar()
+# git push heroku main + heroku ps:scale worker=1 repete esse comando e testar sugerir;
